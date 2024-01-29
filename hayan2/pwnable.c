fe7a5df42a1f72129c4b02639aaf51e74c385e01
hayan2 <uhayan2@gmail.com>
2024-01-29 20:08:56 +0900

    0129-1

--- hayan2/pwnable.c
+++ hayan2/pwnable.c
@@ -1,17 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(void) {
-    char secret[16] = "secret message";
-    char barrier[4];
-    char name[8];
+    // 28 x 30 o
+    char buf[0x28];
 
-    memset(barrier, 0, 4);
+    scanf("%s", buf);
 
-    printf("Your name : ");
-    read(0, name, 12);
-    printf("Your name is %s", name);
-    
+    printf("%d\n%s", strlen(buf), buf);
+
     return 0;
 }
