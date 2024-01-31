b619f84cb06f6595192779000e5abdfa7f72915f
hayan2 <uhayan2@gmail.com>
2024-01-31 18:38:47 +0900

    0131

--- hayan2/11866.c
+++ hayan2/11866.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+int josephus[1001];
+int josephusIndex = 0;
+
 typedef struct Node {
 	int data;
 	struct Node* link;
@@ -11,6 +14,8 @@ typedef struct Node {
 typedef struct Queue {
 	Node* front;
 	Node* rear;
+	Node* current;
+	Node* previous;
 	int size;
 }Queue;
 
@@ -21,49 +26,98 @@ int isQueueEmpty(Queue* queue) {
 void initQueue(Queue* queue) {
 	queue->front = NULL;
 	queue->rear = NULL;
+	queue->current = NULL;
+	queue->previous = NULL;
 	queue->size = 0;
 }
 
 void enQueue(Queue* queue, int data, int endSign) {
 	Node* newNode = (Node*)malloc(sizeof(Node));
 	newNode->data = data;
+	newNode->link = NULL;
 
-	if (endSign == 0) {
-		if (isQueueEmpty(queue)) {
 
-		}
-		else {
-
-		}
+	if (isQueueEmpty(queue)) {
+		queue->front = newNode;
+		queue->rear = newNode;
 	}
 	else {
+		queue->rear->link = newNode;
+		queue->rear = newNode;
+		if (endSign == 1) {
+			newNode->link = queue->front;
+			queue->current = queue->front;
+			queue->previous = queue->rear;
+		}
+	}
 
+	queue->size++;
+}
+
+int deQueue(Queue* queue) {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode = queue->current;
+
+	return;
+}
+
+void getJosephusQuestion(Queue* queue, int N, int K) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < K - 1; j++) {
+			queue->current = queue->current->link;
+			queue->previous = queue->previous->link;
+		}
+		josephus[josephusIndex++] = queue->current->data;
+		queue->previous->link = queue->current->link;
+		queue->current = queue->current->link;
 	}
 }
 
-Node* deQueue(Queue* queue) {
-
+int displayQueue(Queue* queue) {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode = queue->front;
+	for (int i = 0; i < queue->size; i++) {
+		printf("| Data from current node : %d, LINK : %d |\n", newNode->data, newNode->link->data);
+		newNode = newNode->link;
+	}
 }
+
+
 
 int main(void) {
 	Queue queue;
-	Queue josephus;
 	int N, K;
-
-	scanf("%d %d", &N, &K);
 
 	initQueue(&queue);
 
-	for (int i = 0; i < N; i++) {
-		int data;
-		scanf("%d", &data);
-		if (i == N - 1) {
-			enQueue(&queue, data, 1);
+	scanf("%d %d", &N, &K);
+
+	if (N == 1) {
+		printf("<1>");
+		exit(0);
+	}
+
+	for (int i = 1; i < N + 1; i++) {
+		if (N == i) {
+			enQueue(&queue, i, 1);
 		}
 		else {
-			enQueue(&queue, data, 0);
+			enQueue(&queue, i, 0);
 		}
+	}
+
+	getJosephusQuestion(&queue, N, K);
 		
+	for (int i = 0; i < N; i++) {
+		if (i == 0) {
+			printf("<%d, ", josephus[i]);
+		}
+		else if (i == N - 1) {
+			printf("%d>", josephus[i]);
+		}
+		else {
+			printf("%d, ", josephus[i]);
+		}
 	}
 
 	return 0;
