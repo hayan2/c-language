e78f58c4c4cec5edd4807b0b842df2d2e4731ed5
hayan2 <uhayan2@gmail.com>
2024-02-05 12:39:26 +0900

    0205

---
+++ hayan2/24723.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+	int N, res = 1;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		res
+	}
+
+	printf("%d", );
+
+	return 0;
+}
