88338289286937655d77b633ee82da49976c84ed
hayan2 <uhayan2@gmail.com>
2024-02-07 22:10:16 +0900

    1010

--- hayan2/1010.c
+++ hayan2/1010.c
@@ -1,10 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define ARRAY_SIZE 31
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(void) {
-	asdasdasdasd
+	static int arr[ARRAY_SIZE][ARRAY_SIZE];
+	int T;
+
+	scanf("%d", &T);
+
+	for (int i = 1; i < ARRAY_SIZE; i++) {
+		for (int j = 1; j < ARRAY_SIZE; j++) {
+			if (i == j) {
+				arr[i][j] = 1;
+			}
+			else if (i == 1 || j == 1) {
+				arr[i][j] = (i > j) ? i : j;
+			}
+			else {
+				arr[i][j] = arr[i][j - 1] + arr[i - 1][j - 1];
+			}
+		}
+	}
+
+	for (int i = 0; i < T; i++) {
+		int N, M;
+		scanf("%d %d", &N, &M);
+		printf("%d\n", arr[N][M]);
+	}
 
 	return 0;
 }
