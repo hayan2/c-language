ab5e7966cf1826abd4f889295a23a656e39ce52f
hayan2 <uhayan2@gmail.com>
2024-02-10 12:57:30 +0900

    0210

--- hayan2/1037.c
+++ hayan2/1037.c
@@ -1,10 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define ARRAY_SIZE 51
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+int compare(const void* a, const void* b) {
+	return *((int*)a) - *((int*)b);
+}
+
 int main(void) {
-	asdasdasd
+	int arr[ARRAY_SIZE];
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+	
+	qsort(arr, N, sizeof(int), compare);
+
+	printf("%d", arr[0] * arr[N - 1]);
 
 	return 0;
 }
