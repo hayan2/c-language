7a3e41dc61417eeb1e6eed5207b4093a2e8866f3
hayan2 <uhayan2@gmail.com>
2024-06-10 20:53:59 +0900

    0610-1

--- hayan2/10986.c
+++ hayan2/10986.c
@@ -1,19 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define ARRAY_LEN 1000001
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int A[ARRAY_LEN];
-int S[ARRAY_LEN];
+long long S[ARRAY_LEN];
 
-int main(void) {	
-	int N, M;
+
+int main(void) {
+	int N, M, quotCnt = 0;
+	long long res = 0;
 
 	scanf("%d %d", &N, &M);
 	
 	for (int i = 1; i <= N; i++) {
 		scanf("%d", &A[i]);
-		S[i] = S[i - 1] + A[i];
+		if ((S[i - 1] + A[i]) % M == 0) {
+			quotCnt++;
+			res += quotCnt;
+		}
+		S[i] = (S[i - 1] + A[i]) % M;
 	}
+
+	for (int i = 1; i < M; i++) {
+		int cnt = 0;
+		for (int j = 1; j <= N; j++) {
+			if (i == S[j]) {
+				res += cnt;
+				cnt++;
+			}
+		}
+	}
+
+	printf("%lld", res);
 
  	return 0;
 }
