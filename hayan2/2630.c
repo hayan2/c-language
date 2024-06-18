3772bb7ef8eff5dc1161d6312701e11dc5ba3d5b
hayan2 <uhayan2@gmail.com>
2024-06-18 19:03:28 +0900

    0618-1

---
+++ hayan2/2630.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define NxN 128 * 128
+#include <stdio.h>
+
+int arr[NxN] = { 0, };
+int dp[NxN] = { 0, };
+
+int main(void) {
+	int N;
+
+	scanf("%d", &N);
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+
+		}
+	}
+
+	return 0;
+}
