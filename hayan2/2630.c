59cd065fb128d65b4ce5f0c281f6f5274e85b269
hayan2 <uhayan2@gmail.com>
2024-06-22 21:50:03 +0900

    0622

--- hayan2/2630.c
+++ hayan2/2630.c
@@ -1,20 +1,60 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define NxN 128 * 128
+#define ARRAY_LEN 129
+#define WHITE 0
+#define BLUE 1
 #include <stdio.h>
 
-int arr[NxN] = { 0, };
-int dp[NxN] = { 0, };
+int arr[ARRAY_LEN][ARRAY_LEN] = { 0, };
+int white = 0, blue = 0;
+
+void getResult(int x, int y, int N);
 
 int main(void) {
 	int N;
 
 	scanf("%d", &N);
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			scanf("%d", &arr[i][j]);
 		}
 	}
 
+	getResult(0, 0, N);
+
+	printf("%d\n%d", white, blue);
+
 	return 0;
+}
+
+void getResult(int x, int y, int N) {
+	int flagB = 1, flagW = 1;
+
+	for (int i = x; i < x + N; i++) {
+		for (int j = y; j < y + N; j++) {
+			if (arr[i][j] == WHITE) {
+				flagB = 0;
+			}
+			if (arr[i][j] == BLUE) {
+				flagW = 0;
+			}
+		}
+	}
+
+	if (flagB) {
+		blue++;
+
+		return;
+	}
+	if (flagW) {
+		white++;
+
+		return;
+	}
+
+	getResult(x, y, N / 2);
+	getResult(x + N / 2, y, N / 2);
+	getResult(x, y + N / 2, N / 2);
+	getResult(x + N / 2, y + N / 2, N / 2);
+
 }
