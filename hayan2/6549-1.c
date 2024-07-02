ac26661d3e776cb2c558c249df0050c76ee93f24
hayan2 <uhayan2@gmail.com>
2024-07-02 17:19:07 +0900

    0702-2

--- hayan2/6549-1.c
+++ hayan2/6549-1.c
@@ -5,6 +5,8 @@
 #define MIN(a, b) a > b ? b : a
 #include <stdio.h>
 
+int cnt = 0;
+
 ull hist[ARRAY_LEN] = { 0, };
 
 ull getResult(ull low, ull high);
@@ -27,27 +29,31 @@ int main(void) {
 }
 
 ull getResult(ull low, ull high) {
+	printf("cnt : %d\n", cnt++);
+	printf("low : %lld high : %lld\n", low, high);
 	ull mid = (low + high) / 2;
-	ull midL = mid - 1, midH = mid + 1;
+	ull midL = mid, midH = mid;
 	ull midArea = hist[mid], height = hist[mid];
+	ull width;
 
 	if (low + 1 >= high) {
 		return midArea * 1;
 	}
 
-	while (midL > low || midH < high) {
-		if (midL >= low && hist[midL] <= hist[midL - 1]) {
-			height = MIN(height, hist[midL - 1]);
-			midL--;
-		}
-		if (midH <= high && hist[midH] <= hist[midH + 1]) {
-			height = MIN(height, hist[midH + 1]);
-			midH++;
-		}
-		
-		ull width = midH - midL - 1;
+	while (midL >= low) {
+		height = MIN(height, hist[midL]);
+		width = midH - midL + 1;
 		midArea = MAX(midArea, width * height);
+		midL--;
 	}
 
-	return MAX(midArea, MAX(getResult(low, mid), getResult(mid, high)));
+	while (midH <= high) {
+		height = MIN(height, hist[midH]);
+		width = midH - midL + 1;
+		midArea = MAX(midArea, width * height);
+
+		midH++;
+	}
+
+	return MAX(midArea, MAX(getResult(low, mid - 1), getResult(mid + 1, high)));
 }
