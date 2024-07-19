4934a0b9a1642cb104134cf1b347d8a823137b97
hayan2 <uhayan2@gmail.com>
2024-07-19 17:24:11 +0900

    0719

--- hayan2/11066.c
+++ hayan2/11066.c
@@ -1,32 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define MIN(a, b) a > b ? b : a
+#define uint unsigned int
 #define DEFAULT 2147483641
 #define ARRAY_LEN 501
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
-	int dp[ARRAY_LEN][ARRAY_LEN];
-	int arr[ARRAY_LEN], cost[ARRAY_LEN];
-	int N;
+	int dp[ARRAY_LEN][ARRAY_LEN] = { 0, };
+	int psum[ARRAY_LEN] = { 0, };
+	int N, x, T;
 	
-	scanf("%d", &N);
+	for (scanf("%d", &T); T--;) {
+		scanf("%d", &N);
 
-	for (int i = 1; i <= N; i++) {
-		scanf("%d", &arr[i]);
-		dp[i][i] = arr[i];
-	}
+		for (int i = 1; i <= N; i++) {
+			scanf("%d", &x);
+			psum[i] = psum[i - 1] + x;
+		}
 
-	// dp[i][j] = MIN(dp[i][j], dp[i][k] + dp[k + 1][j])
-	for (int i = 1; i < N; i++) {
-		for (int j = i + 1; j <= N; j++) {
-			dp[i][j] = DEFAULT;
+		// dp[i][j] = MIN(dp[i][j], dp[i][k] + dp[k + 1][j]) ?
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				dp[j][i + j] = DEFAULT;
 
-			for (int k = i; k <= j; k++) {
-
+				for (int k = j; k <= j + i; k++) {
+					dp[j][i + j] = MIN(dp[j][i + j], dp[j][k] + dp[k + 1][i + j] + psum[i + j] - psum[j - 1]);
+				}
 			}
 		}
+		printf("%d\n", dp[1][N]);
 	}
+
+	
 
 	return 0;
 }
