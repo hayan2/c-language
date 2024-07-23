115cb2e3ce8f573af8b79c8b32162c7769392005
hayan2 <uhayan2@gmail.com>
2024-07-23 17:13:40 +0900

    0723

---
+++ hayan2/11049.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define ARRAY_LEN 501
+#include <stdio.h>
+
+int main(void) {
+	int dp[ARRAY_LEN][ARRAY_LEN] = { 0, };
+	int N, r, c;
+
+	scanf("%d", &N);
+	
+	for (int i = 0; i < N; i++) {
+		
+	}
+
+
+	return 0;
+}
