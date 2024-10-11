6a9ce844a8aa2b716fe5a4f03bbc572406e5f688
hayan2 <uhayan2@gmail.com>
2024-10-11 20:36:29 +0900

    1011-2

--- hayan2/9370.cpp
+++ hayan2/9370.cpp
@@ -1,15 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define INF 1e9
 #define MAX_V 2001
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 // in order from left,
 // vertex, edge, destination options, artist origin, the road passed
-int v, e, t, s, g, h;
-vector<pair<int, int>> graph[MAX_V];
+int V, E, t, s, g, h;
+
+int sol(vector<pair<int, int>> *graph, int u, int v) {
+	vector<int> ret(MAX_V, INF);
+	priority_queue<pair<int, int>> heap;
+
+
+
+	return ret[v];
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
@@ -19,12 +29,19 @@ int main(void) {
 	cin >> T;
 
 	while (T--) {
-		cin >> v >> e >> t;
+		vector<pair<int, int>> graph[MAX_V];
+		cin >> V >> E >> t;
 		cin >> s >> g >> h;
+		int u, v, w;
 
-		for (int i = 0; i < e; i++) {
-
+		for (int i = 0; i < E; i++) {
+			cin >> u >> v >> w;
+			
+			graph[u].push_back({ v, w });
+			graph[v].push_back({ u, w });
 		}
+		
+				
 	}
 
 	return 0;
